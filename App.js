69684d2f58c7ec1c11c1bd762aca8b4596666b64
cskuncan <cskuncan@gmail.com>
2021-06-30 17:56:00 +0200

    zero-day

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Smartest shopping list . . . </Text>
+      <Text>Notifications . . . </Text>
+      <Text>Cost effective . . . </Text>
+      <Text>Sharing foods . . . </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
